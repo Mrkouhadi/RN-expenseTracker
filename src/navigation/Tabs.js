08fe3b614a8b1156e2c2c55a549117d470b30259
mrkouhadi <teacherbryan@outlook.com>
2022-04-04 02:23:30 +0800

    editing the tabs icons

--- src/navigation/Tabs.js
+++ src/navigation/Tabs.js
@@ -29,13 +29,13 @@ const Tabs=()=> {
       <Tab.Screen name="RecentExpenses" component={RecentExpenses} options={{ 
         tabBarIcon: ({focused}) => (<View style={{alignItems:'center',justifyContent:'center',top:Platform.OS === 'ios'?15:0}}>
                               <Icon name="appstore1" color={focused ? Colors.primaryColor:Colors.fontColor} size={25} />
-                              {focused && <Text style={{color:Colors.primaryColor,marginTop:8 }}>Profile</Text>}
+                              {focused && <Text style={{color:Colors.primaryColor,marginTop:8 }}>All Expenses</Text>}
                           </View> )
         }}/>
       <Tab.Screen name="AllExpenses" component={AllExpenses} options={{ 
         tabBarIcon: ({focused}) => (<View style={{alignItems:'center',justifyContent:'center',top:Platform.OS === 'ios'?15:0}}>
-                              <Ionicons name="chatbox-ellipses-outline" color={focused ? Colors.primaryColor:Colors.fontColor} size={25} />
-                              {focused && <Text style={{color:Colors.primaryColor,marginTop:8 }}>Chat</Text>}
+                              <Ionicons name="reader" color={focused ? Colors.primaryColor:Colors.fontColor} size={25} />
+                              {focused && <Text style={{color:Colors.primaryColor,marginTop:8 }}>Recent Expenses</Text>}
                           </View> )
         }}/>
     </Tab.Navigator>
