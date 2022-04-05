7176c152501f0e388963b9234a7414fa8d619d85
mrkouhadi <teacherbryan@outlook.com>
2022-04-05 23:36:03 +0800

    adding tittle to the output of expenses

--- src/components/expensesOutput/ExpensesOutput.js
+++ src/components/expensesOutput/ExpensesOutput.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { View } from 'react-native'
 
 const ExpensesOutput = () => {
-  return (
-    <View>
-        <Text>Expenses Output</Text>
+  return <View>
+            <View>
+                <Text>In Last 7 Days</Text>
+                <Text> $ 367.34</Text>
+            </View>
     </View>
-  )
 }
 
 export default ExpensesOutput
