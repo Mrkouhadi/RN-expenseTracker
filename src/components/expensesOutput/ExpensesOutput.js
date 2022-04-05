cfa8554ce8f142e0c910bc54f8e9c6aa15a8f54e
mrkouhadi <teacherbryan@outlook.com>
2022-04-05 23:30:54 +0800

    adding expensesOutput component

---
+++ src/components/expensesOutput/ExpensesOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const ExpensesOutput = () => {
+  return (
+    <View>
+        <Text>Expenses Output</Text>
+    </View>
+  )
+}
+
+export default ExpensesOutput
