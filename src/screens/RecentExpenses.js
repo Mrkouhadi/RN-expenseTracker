24ccb0e8a37f3e8cf29f47428d8f8efdce56c5e4
mrkouhadi <teacherbryan@outlook.com>
2022-04-11 00:41:37 +0800

    adding loading component when fetching data

--- src/screens/RecentExpenses.js
+++ src/screens/RecentExpenses.js
@@ -1,15 +1,19 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ExpensesOutput from '../components/expensesOutput/ExpensesOutput';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { ExpensesCtx } from '../store/Expenses-ctx';
 import { getDateMinusDays } from '../util/date';
 import { fetchExpensesFromDb } from '../util/http';
 
 const RecentExpenses = (props) => {
+  const [isFetching, setIsFetching] = useState(true);
         const ourExpenseCtx = useContext(ExpensesCtx);
 
         useEffect(()=>{
           async function fetch(){
+            setIsFetching(true);
             const expenses = await fetchExpensesFromDb();
+            setIsFetching(false);
             ourExpenseCtx.setExpenses(expenses);
           }
           fetch();
@@ -21,6 +25,8 @@ const RecentExpenses = (props) => {
 
           return (expense.date >= last7days) && (expense.date <= today);
         })
+
+        if(isFetching) return <LoadingOverlay />
   return<ExpensesOutput fallbackText="No Recenet Expenses Yet !!!" expenses={recentExpenses} expensesPeriod="Last 7 Days"/>
 };
 export default RecentExpenses
